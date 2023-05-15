ff90740a691218cb9ed1124f003976335b19e672
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-05-15 10:08:56 -0300

    15/05

---
+++ javascript-v1/map.js
@@ -0,0 +1,12 @@
+// var linguagens = ['js' , 'HTML', 'CSS' , 'PHP' , 'React']
+
+// linguagens.map((e , i)=>{
+//     console.log(e , i)
+// }
+// )
+
+const dobrar =(e)=>e/2
+
+var num = ['1' , '2' , '3' , '4' , '5'].map(dobrar)
+
+console.log(num)
---
+++ javascript-v1/this.js
@@ -0,0 +1,18 @@
+// function aluno(nome , nota){
+//     console.log(nome)
+//     console.log(nota)
+
+
+// aluno( 'adson' , 100)
+
+// dados_arrow=function(){
+//     setTimeout(()=>{
+
+//     console.log(nome)
+//     console.log(nota)
+    
+//     }, 2000)
+// }
+// }
+// aluno('adson' , 5)
+
