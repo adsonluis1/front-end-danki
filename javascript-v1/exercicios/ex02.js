524fed2e02c72d40fb3aac5a00e37c477465c157
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-05-18 11:52:13 -0300

    18/05

--- javascript-v1/exercicios/ex02.js
+++ javascript-v1/exercicios/ex02.js
@@ -1,11 +1,27 @@
-var tela_curso = document.querySelector('#cursos')
-const text_curso = document.querySelector('#icurso')
+var text_curso = document.querySelector('#icursos')
 var b1 = document.querySelector('#b1')
-const b2 = document.querySelector('#b2')
-const b3 = document.querySelector('#b3')
+var b2 = document.querySelector('#b2')
+var b3 = document.querySelector('#b3')
+var tela_curso = document.querySelector('#cursos')
+var radio = [...document.getElementsByName('radio1')]
+    
+    
 
-b1.addEventListener('click' , ()=>{
-    alert('aaaaaaaaaa')
-})
+    b3.addEventListener('click' , (evt)=>{
 
+    const selecionados = radio.filter((el , i)=>{
 
+        return el.checked
+
+        })
+    
+    })
+
+    b2.addEventListener('click' , (evt)=>{
+        const selecionado = radio.filter((el)=>{
+            return el.target
+        })
+
+        tela_curso.removeChild(selecionado)
+
+    })
