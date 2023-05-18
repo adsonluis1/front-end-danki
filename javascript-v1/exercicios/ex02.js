5ac0c51d9a812753b0291290ee171920313f17e4
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-05-18 09:35:10 -0300

    18/05

---
+++ javascript-v1/exercicios/ex02.js
@@ -0,0 +1,11 @@
+var tela_curso = document.querySelector('#cursos')
+const text_curso = document.querySelector('#icurso')
+var b1 = document.querySelector('#b1')
+const b2 = document.querySelector('#b2')
+const b3 = document.querySelector('#b3')
+
+b1.addEventListener('click' , ()=>{
+    alert('aaaaaaaaaa')
+})
+
+
