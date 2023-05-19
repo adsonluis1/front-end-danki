6e21a60fce65097313e69941ec976ed414571c2f
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-05-19 17:06:06 -0300

    19/05

--- javascript-v1/exercicios/ex02.js
+++ javascript-v1/exercicios/ex02.js
@@ -1,27 +1,59 @@
-var text_curso = document.querySelector('#icursos')
+var text_curso = document.querySelector('#icurso')
 var b1 = document.querySelector('#b1')
 var b2 = document.querySelector('#b2')
 var b3 = document.querySelector('#b3')
 var tela_curso = document.querySelector('#cursos')
 var radio = [...document.getElementsByName('radio1')]
-    
-    
+var tela = document.querySelector('#tela')
+var articl = document.getElementById('tela')
+var a_button = document.querySelectorAll('.a_button')
 
-    b3.addEventListener('click' , (evt)=>{
+b3.addEventListener('click' , (evt)=>{
 
     const selecionados = radio.filter((el , i)=>{
-
         return el.checked
+        })
+        
+        selecionados.map((e)=>{
+                let div = e.parentNode
+                
+                var linguagem = div.firstChild.textContent
+               
+                alert('curso escolhido ↓     ' + linguagem + '  ↑')
+                
+        })
+        
+    })
+
+b2.addEventListener('click' , ()=>{
+
+    const selecionados = radio.filter((el , i)=>{
+        return el.checked
+        })
+
+        selecionados.map((e)=>{
+
+            let div = e.parentNode
+            
+            div.remove()
 
         })
+
+})
+
+const criarcurso = ()=>{
+    var div = document.createElement('div')
+    div.setAttribute('input' , 'radio')
+    div.setAttribute('class' , 'a_button')
+    div.setAttribute('id' , 'a')
+    articl.appendChild(div)
     
-    })
+}
 
-    b2.addEventListener('click' , (evt)=>{
-        const selecionado = radio.filter((el)=>{
-            return el.target
-        })
+b1.addEventListener('click' , ()=>{
+    criarcurso()
+    a_button.innerText= text_curso.value
+})
 
-        tela_curso.removeChild(selecionado)
 
-    })
+
