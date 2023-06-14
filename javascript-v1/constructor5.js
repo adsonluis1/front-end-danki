701df5037dc517017ffe022250df8192f23dec10
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-06-14 15:01:00 -0300

    14/06

--- javascript-v1/constructor5.js
+++ javascript-v1/constructor5.js
@@ -1,3 +1,8 @@
+const inome = document.getElementById('inome')
+const iidade = document.getElementById('iidade')
+const btn_click = document.getElementById('click')
+const tela = document.getElementById('tela')
+
 class pessoas{
     constructor(inome , iidade){
         this.nome = inome
@@ -5,54 +10,30 @@ class pessoas{
     }
 }
 
-const inome = document.getElementById('inome')
-const iidade = document.getElementById('iidade')
-const btn_click = document.getElementById('click')
-const tela = document.getElementById('tela')
-
-let p= []
-let nome=[]
-let nome2
-let idade = []
-let idade2
-let v = false
-btn_click.addEventListener('click' , (evt)=>{
-    // if(inome.value == '' || iidade.value == ''){
-    //     alert('preencha corretamente as lacunas')
-    // }else{
-    tela.innerHTML= ''
-    let hehe = (new pessoas(inome.value , iidade.value))
-    if(v == false){
-    p.push(hehe)
-    }
-    console.log(p)
-    p.map((e)=>{
-        nome2 =e.nome 
-    })
-    if(nome.indexOf(inome.value) != -1){
-        alert('aaaaaaaaaaaaaaa')
+let array_nomes = []
+let p = []
+btn_click.addEventListener('click' , ()=>{
+    if(inome.value == '' || iidade.value == ''){
+        alert('preencha as lacunas corretamente')
     }else{
-    nome.push(nome2)
-    // nome.map((e)=>{
-    //     console.log(`nome: ${e}`)
-        
-    // })
-    if(v == true){
-        p.push(hehe)
-    }
-    v = true
-
-    }
-
-    const div = document.createElement('div')
-    div.setAttribute('class' , 'div')
-    // div.setAttribute('id' , 'div')
-    tela.appendChild(div)
-
-    p.map((e)=>{
-        // div.innerHTML+= e.nome +'<br>'
-        // div.innerHTML+= e.idade +'<br>'
-        })
     
-    
+        if(array_nomes.indexOf(inome.value) != -1){
+            alert('numero reptido')
+        }else{
+            tela.innerHTML= ''
+            let hehe= new pessoas(inome.value, iidade.value)
+            p.push(hehe)
+            p.forEach((e)=>{
+            const div= document.createElement('div')
+            div.setAttribute('class' , 'div')
+            tela.appendChild(div)
+            array_nomes.push(inome.value)
+            console.log(e)
+            div.innerHTML+='nome: ' + e.nome + '<br>'
+            div.innerHTML+='idade: ' + e.idade + '<br>'
+    })
+        console.log(p)
+       
+    }
+}
 })
