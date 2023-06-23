e96e0e59f4e78eb719721a1622305c3c0ad67e94
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-06-23 09:06:37 -0300

    23/06

--- javascript-v1/brincadeira/roletaimg/cronometro/cronometro.js
+++ javascript-v1/brincadeira/roletaimg/cronometro/cronometro.js
@@ -9,9 +9,12 @@ const pisca = document.getElementById('pisca')
 
 btn_setar.addEventListener('click' , (evt)=>{
     hehe.classList.add('hehe1')
-
-    if(hehe.className== 'hehe1'){
-    btn_go.addEventListener('click' , ()=>{
+})
+btn_go.addEventListener('click' , ()=>{
+if(hehe.className== 'hehe1'){
+    if(temp.value == 0){
+        tela.innerHTML= 'ERRO'
+    }else{
         let tempS= temp.value 
         tela.innerHTML= tempS
         let interval =setInterval(()=>{
@@ -30,12 +33,13 @@ btn_setar.addEventListener('click' , (evt)=>{
             },1000)
         },temp.value *1000)    
         temp.value = ''
+    }
+    }
 
 
     btn_reset.addEventListener('click' , (e)=>{
         clearInterval(interval2)
     })
     })
-   
-    }
-})
+
+
