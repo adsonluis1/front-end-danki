db5ea751de8511f18338dbf47f373c0d20ec50e4
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-06-28 20:13:27 -0300

    28/06

--- javascript-v1/animação/animação2.js
+++ javascript-v1/animação/animação2.js
@@ -1,8 +1,52 @@
-const btn_play = document.getElementById('btn_play')
+const btn_play= document.getElementById('btn_direita')
+const btn_parar = document.getElementById('btn_esquerda')
 const carro = document.getElementById('carro')
-const btn_parar = document.getElementById('btn_parar')
 
-let width = window.innerWidth - 40
+let x = 0
+let width= window.innerWidth
+let interval2= 0
+let pode= (num)=>{
+    if(num >= 1){
+            if(x <= width-40){
+            x+= 10 
+            carro.style.left=x+'px'
+            }
+    }else if(num <=-1){
+            if(x >=0){
+            x+= -10 
+            carro.style.left=x+'px'
+            }
+    }
+}
+
+window.addEventListener('resize' , ()=>{
+    width= window.innerWidth
+    console.log(width)
+})
+
+let check= ()=>{
+    if(x >= width-40){
+        clearInterval(interval)
+        interval= setInterval(pode,50,-1)
+    }
+    if(x <= 0){
+        clearInterval(interval)
+        interval= setInterval(pode,50,1)
+       
+    }
+}
+
+let interval = null
+btn_play.addEventListener('click',()=>{
+    
+    interval2= setInterval(check,100)
+})
+
+btn_parar.addEventListener('click',()=>{
+   clearInterval(interval2)
+
+    
+})
 
 
 
