4d47ba442f34cd5360e20b6efbc44d6240d27179
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-07-26 20:15:23 -0300

    ajeitando

--- javascript-v1/fetch/junçãoComGraficos/index.js
+++ javascript-v1/fetch/junçãoComGraficos/index.js
@@ -1,22 +1,7 @@
 const ctx = document.getElementById('grafico');
-const url = 'https://teste.sradson.repl.co'
 const num = []
-const div_grafico = document.querySelector('#div_grafico')
-fetch(url)
-.then(res=>res.json())
-.then(res=>{
-    num.push(res.temperatura)
-    num.push(res.pressao)
-    num.push(res.nivel)
-    console.log(num)
-})
 
-
-
-setTimeout(()=>{
-    div_grafico.style.display= 'block'
-},3000)
-new Chart(ctx, {
+let grafico1 = new Chart(ctx, {
 type: 'bar',
 data: {
     labels: ['temperatura' , 'pressão' , 'nivel'],
@@ -35,3 +20,14 @@ options: {
 }
 });
 
+const url = 'https://teste.sradson.repl.co'
+const div_grafico = document.querySelector('#div_grafico')
+fetch(url)
+.then(res=>res.json())
+.then(res=>{
+    num.push(res.temperatura)
+    num.push(res.pressao)
+    num.push(res.nivel)
+    grafico1.update()
+    console.log(num)
+})
