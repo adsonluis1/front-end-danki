57205d5762249e7a127ebcacf251a0a8de56f9d4
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-07-27 19:46:22 -0300

    pt2 finalizada

--- javascript-v1/timestamp/pt2.js
+++ javascript-v1/timestamp/pt2.js
@@ -1,19 +1,59 @@
 const tela = document.querySelector('#tela')
-const temp = Date.now()
-let tempSeg = null
-const contador=()=>{
-    const temp2= Date.now()
-    let atual= temp2-temp
-    tempSeg = Math.floor(atual/1000)
-    tela.innerHTML= mostrador()
-}
+const play = document.querySelector('#play')
+const pause = document.querySelector('#pause')
+const zerar = document.querySelector('#zerar')
+const cortar = document.querySelector('#cortar')
+const mestre = document.querySelector('#mestre')
+let temp = null
+let interval = null
+let pode = true
+let temp2 = null
+let form = null
+let num = 1
 
-const mostrador=()=>{
-    const tempHora= Math.floor(tempSeg/3600)
-    const resto = tempSeg%3600
-    const tempMin= Math.floor(resto/60)
-    const segundos = Math.floor(resto%60)
-    let form = `${(tempHora<10?'0'+tempHora:tempHora)}:${tempMin<10?'0'+tempMin:tempMin}:${(segundos<10?'0'+segundos:segundos)}`
-    return form
+play.addEventListener('click', ()=>{
+    temp = Date.now()
+    let tempSeg = null
+
+    const contador=()=>{
+       temp2= Date.now()
+        let atual= temp2-temp
+        tempSeg = Math.floor(atual/1000)
+        tela.innerHTML= mostrador()
+    }
+
+    const mostrador=()=>{
+        console.log(temp)
+        const tempHora= Math.floor(tempSeg/3600)
+        const resto = tempSeg%3600
+        const tempMin= Math.floor(resto/60)
+        const segundos = Math.floor(resto%60)
+        form = `${(tempHora<10?'0'+tempHora:tempHora)}:${tempMin<10?'0'+tempMin:tempMin}:${(segundos<10?'0'+segundos:segundos)}`
+        return form
 }
-setInterval(contador,1000)
+    interval = setInterval(contador,1000)
+})
+
+pause.addEventListener('click' , ()=>{
+    clearInterval(interval)
+})
+
+zerar.addEventListener('click' , ()=>{
+    clearInterval(interval)
+    tela.innerHTML= '00:00:00'
+    form = '00:00:00'
+    mestre.innerHTML=''
+})
+
+cortar.addEventListener('click' , ()=>{
+    const div = document.createElement('div')
+    div.setAttribute('class' , 'cronometro')
+    const label = document.createElement('label')
+    label.innerHTML= `corrida ${num}`
+    const p = document.createElement('p')
+    mestre.appendChild(div)
+    div.appendChild(label)
+    div.appendChild(p)
+    p.innerHTML= (form==null?'00:00:00':form)
+    num++
+})
