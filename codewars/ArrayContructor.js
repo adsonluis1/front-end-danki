d1ccfaca12a59d6ab3d435f21d7bdb07c649136c
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2024-02-19 16:35:02 -0300

    Up arrayConstructor

--- codewars/ArrayContructor.js
+++ codewars/ArrayContructor.js
@@ -1,3 +1,10 @@
 const arr = new Array(10)
-const arr2 = Array.from(('adson' + 'luis'),(evt,i) => evt)
-console.log(arr2.sort().join(''))
+const arr2 = Array.from(('adson' + 'luis'),(evt,i) => [...new Set(evt)]).join('')
+console.log(arr2)
+
+function tirandoDuplicadas(fistName, secondName){
+    let arr = fistName.split('').concat(secondName.split(''))
+    return [...new Set(arr)].sort().join('')
+}
+
+console.log(tirandoDuplicadas('adson','luisss'))
