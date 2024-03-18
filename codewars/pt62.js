17b68c930f04df7a5fbb892fc1eea5d29cfe34f3
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2024-03-18 11:39:16 -0300

    pt53

--- codewars/pt62.js
+++ codewars/pt62.js
@@ -1,5 +1,5 @@
 function descendingOrder(n){
     return +n.toString().split('').sort((a,b)=> b-a).join('')
   }
-
+  
   console.log(descendingOrder(123))
---
+++ codewars/pt63.js
@@ -0,0 +1,5 @@
+function lovefunc(f1, f2){
+    return f1%2==0?f2%2==0?false:true:f2%2==0?true:false
+  }
+
+  console.log(lovefunc(6,3))
