59f439977b253e5c7ead8b587bb5c67daf35f845
adsonluis1 <dev.adsonluis@gmail.com>
2024-06-14 12:15:47 -0300

    renderizando salada final

--- javascript-v1/Poo/aula2/showSalada.js
+++ javascript-v1/Poo/aula2/showSalada.js
@@ -1,3 +1,23 @@
-function createDivSalada (element, salada){
-    
+const main = document.querySelector('main')
+const section = document.createElement('section')
+
+function createDivSalada (salada){
+    console.log(salada)
+    const ingredientes = []
+    salada.ingredientes.map((e)=>{
+        ingredientes.push(e.nome)
+    })
+    ingredientes.sort()
+    section.innerHTML= ` 
+        <div>
+            <h1>Detalhes</h1>
+            <h2>Tamanho:${salada.tamanho}</h2>
+            <h2>Ingredientes: ${ingredientes.join(', ')}</h2>
+            <h2>Preço:${salada.preco}</h2>
+            <h2>Obervação:${salada.observacao}</h2>
+        </div>
+    `
+    main.appendChild(section)
 }
+
+export default createDivSalada
