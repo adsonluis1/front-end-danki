1f146a4092013de5bc6e3de89ad8c50b579c7234
adsonluis1 <dev.adsonluis@gmail.com>
2024-06-13 23:31:11 -0300

    ingredientes

---
+++ javascript-v1/Poo/aula2/incredientes.js
@@ -0,0 +1,17 @@
+const ingredientes = [
+    {
+        nome:'tomate',
+        preco: 3.5
+    },{
+        nome:'alface',
+        preco: 2.5
+    },{
+        nome:'pepino',
+        preco: 4
+    },{
+        nome:'azeitona',
+        preco: 0.5
+    }
+]
+
+export default ingredientes
